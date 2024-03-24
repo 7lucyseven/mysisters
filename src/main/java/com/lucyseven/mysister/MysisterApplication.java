286079ea5487da72888729431329de5848d6bf44
lucyseven <7lucyseven@gmail.com>
2024-03-24 17:05:43 +0900

    initial commit

---
+++ src/main/java/com/lucyseven/mysister/MysisterApplication.java
@@ -0,0 +1,13 @@
+package com.lucyseven.mysister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysisterApplication.class, args);
+	}
+
+}
