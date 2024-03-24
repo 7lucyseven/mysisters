66c698e8c10db7afac6c2caa5d8700fc794f8532
lucyseven <7lucyseven@gmail.com>
2024-03-24 17:21:39 +0900

    fixed typo

---
+++ src/main/java/com/lucyseven/mysisters/MysistersApplication.java
@@ -0,0 +1,13 @@
+package com.lucyseven.mysisters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysistersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysistersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucyseven/mysisters/MysistersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucyseven.mysisters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysistersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
